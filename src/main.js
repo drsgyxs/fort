7e468a761b0eeda16f13109d5e472820536f67a7
drsg <yaoxs1949@foxmail.com>
2020-07-10 10:35:12 +0800

    创建项目

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import { Button, Alert, ConfigProvider } from 'ant-design-vue';
 
 Vue.config.productionTip = false;
+
+Vue.use(Button);
+Vue.use(ConfigProvider).use(Alert);
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app');
